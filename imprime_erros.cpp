1050ae4284d9e6d8ac4fc9e9e79bbc3f3031dd4e
KaioSantos32 <53626595+KaioSantos32@users.noreply.github.com>
2022-05-19 11:43:54 -0300

    Add files via upload

---
+++ imprime_erros.cpp
@@ -0,0 +1,11 @@
+#include "imprime_erros.hpp"
+#include <iostream>
+
+
+void imprime_erros(std::vector<char> chutes_errados){
+    std::cout << "Chutes errados: ";
+    for(char letra: chutes_errados){
+        std::cout << letra << " ";
+    }
+    std::cout << std::endl;
+}
